42fcde17b840215f1d708377e1306ef9ece7139b
Ryan <lmhyuks@gmail.com>
2019-03-10 21:06:54 +0900

    2일차 스터디

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
+        name: 'Vue',
+        feelsgood: 'https://avatars3.githubusercontent.com/u/6947055',
+        rabbit: true,
         value: 0
     }
 });
