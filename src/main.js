3474f15e682114d72ea538094af12899281ef31c
Ryan <lmhyuks@gmail.com>
2019-03-10 21:35:28 +0900

    3회차 스터디

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
+        todos: [
+            { text: 'Vue.js 튜토리얼 작성하기' },
+            { text: 'Webpack2 알아보기'},
+            { text: '사이드 프로젝트 진행하기' }
+        ],
         name: 'Vue',
         feelsgood: 'https://avatars3.githubusercontent.com/u/6947055',
         rabbit: true,
